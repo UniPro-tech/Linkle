4957cea89c89bf543719bcd92bc485c3bbffce16
yuito-it <yuito@yuito-it.jp>
2025-03-23 00:27:52 +0900

    :bug: 時間フォーマット関数において、時間と分のゼロパディングを追加

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -3,8 +3,18 @@ export const timeFormat = (dateRaw: Date, format: string) => {
   format = format.replace(/YYYY/, date.getFullYear().toString());
   format = format.replace(/MM/, (date.getMonth() + 1).toString());
   format = format.replace(/DD/, date.getDate().toString());
-  format = format.replace(/hh/, date.getHours().toString());
-  format = format.replace(/mm/, date.getMinutes().toString());
+  format = format.replace(
+    /hh/,
+    date.getHours().toString().length < 2
+      ? "0" + date.getHours().toString()
+      : date.getHours().toString()
+  );
+  format = format.replace(
+    /mm/,
+    date.getMinutes().toString().length < 2
+      ? "0" + date.getMinutes().toString()
+      : date.getMinutes().toString()
+  );
   format = format.replace(/ss/, date.getSeconds().toString());
 
   return format;
